2cffbc10c3d9753e972461f233539c0162f5d6e1
Ripon Chandra Saha <riponsaha199@gmail.com>
2025-10-05 22:25:15 +0600

    added a project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+
 import Navbar from "./components/Navbar/Navbar";
 import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
-import Experience from "./components/Experience/Experience";
+/* import Experience from "./components/Experience/Experience"; */
 import Work from "./components/Work/Work";
 import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
@@ -22,7 +22,7 @@ const App = () => {
         <Navbar />
         <About />
         <Skills />
-        <Experience />
+       {/*  <Experience /> */}
         <Work />
         <Education />
 
--- src/constants.js
+++ src/constants.js
@@ -44,7 +44,7 @@ import bsaLogo from './assets/education_logo/ewulogo.png';
 /* import vpsLogo from './assets/education_logo/vps_logo.png'; */
 
 // Project Section Logo's
-import githubdetLogo from './assets/work_logo/github_det.png';
+import gocart from './assets/work_logo/gocart.jpg';
 import csprepLogo from './assets/work_logo/cs_prep.png';
 import movierecLogo from './assets/work_logo/movie_rec.png';
 import taskremLogo from './assets/work_logo/task_rem.png';
@@ -216,16 +216,17 @@ export const SkillsInfo = [
   ];
 
   export const projects = [
-    /* {
+    {
       id: 0,
-      title: "GitHub Profile Detective",
+
+      title: "Gocart E-Commerce Website",
       description:
-        "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
-      image: githubdetLogo,
-      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
-      github: "https://github.com/codingmastr/GitHub-Profile-Search-App-Using-React-JS",
-      webapp: "https://githubprofiledetective.netlify.app/",
-    }, */
+        "Gocart is a modern e-commerce website built with Next.js, Express, and MongoDB. It features secure Stripe payments, Clerk authentication, admin role management, and a responsive user interface optimized for performance and SEO.",
+      image: gocart,
+      tags: ["HTML", "CSS", "JavaScript", "Next JS", "API", "Prisma", "Clerk","Express JS","MongoDB"],
+      github: "https://github.com/riponchandrasaha/gocart",
+      webapp: "https://gocart-lake.vercel.app/",
+    },
    /*  {
       id: 1,
       title: "CS Prep",
